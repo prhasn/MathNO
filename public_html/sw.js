c6b2787d6389995958d30d1ace9bd4b700a47402
prhasn <hasn@degreesign.com>
2025-03-12 22:04:20 +0700

    logo update

--- public_html/sw.js
+++ public_html/sw.js
@@ -5,8 +5,8 @@ const cacheName = `mathno_calculator`;
 self.oninstall = e => e.waitUntil(
 	caches.open(cacheName).then(files =>
 		files.addAll([
-			`./index.html?25031204`,
-			`./manifest.json?25031204`,
+			`./index.html?25031205`,
+			`./manifest.json?25031205`,
 			`./sw.js`
 		]).then(() => self.skipWaiting()))
 );
